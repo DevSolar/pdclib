779d1ca728f28d1c493c1a7a4fc3279da3824bb6
Martin Baute <solar@rootdirectory.de>
2004-01-18 14:39:11 +0000

    Implemented.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@17 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/string/memchr.c
+++ functions/string/memchr.c
@@ -5,29 +5,16 @@
 // This code is Public Domain. Use, modify, and redistribute at will.
 // ----------------------------------------------------------------------------
 
-// ----------------------------------------------------------------------------
-// C++
-
-const void * memchr( const void * s, int c, size_t n ) { /* TODO */ };
-void * memchr( void * s, int c, size_t n ) { /* TODO */ };
-
-// ----------------------------------------------------------------------------
-// Standard C
-
-void * memchr( const void * s, int c, size_t n ) { /* TODO */ };
-
-/* PDPC code - unreviewed
+void * memchr( const void * src, int c, size_t n )
 {
-    const unsigned char *p;
-    size_t x = 0;
-    
-    p = (const unsigned char *)s;
-    while (x < n)
+    const unsigned char * p = (const unsigned char *) src;
+    while ( n-- )
     {
-        if (*p == (unsigned char)c) return ((void *)p);
-        p++;
-        x++;
+        if ( *p == (unsigned char) c )
+        {
+            return (void *) p;
+        }
+        ++p;
     }
-    return (NULL);
+    return NULL;
 }
-*/
