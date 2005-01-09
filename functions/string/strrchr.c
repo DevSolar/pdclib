29843710bd367058f0afde024be2212751de3645
Martin Baute <solar@rootdirectory.de>
2005-01-09 13:49:21 +0000

    Added search functions.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@49 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/string/strrchr.c
@@ -0,0 +1,25 @@
+/* $Id$ */
+
+/* Release $Name$ */
+
+/* strrchr( const char *, int c )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <string.h>
+
+char * strrchr( const char * s, int c )
+{
+    size_t i = 0;
+    while ( p[i++] );
+    do
+    {
+        if ( p[--i] == (char) c )
+        {
+            return (char *) p + i;
+        }
+    } while ( i );
+    return NULL;
+}
