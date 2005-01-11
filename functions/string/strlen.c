68dffc782d0148da4d66c23187bc18b5f140183c
Martin Baute <solar@rootdirectory.de>
2005-01-11 06:43:44 +0000

    Initial checkin.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@51 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/string/strlen.c
@@ -0,0 +1,23 @@
+/* $Id$ */
+
+/* Release $Name$ */
+
+/* strlen( const char * )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <string.h>
+
+size_t strlen( const char * s )
+{
+    size_t rc = 0;
+    while ( src[rc] )
+    {
+        ++rc;
+    }
+    return rc;
+}
+
+#warning Test driver missing.
