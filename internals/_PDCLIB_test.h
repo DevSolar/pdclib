e9578ea1199a431969edd212a1b917ac0c64b326
Martin Baute <solar@rootdirectory.de>
2005-11-21 18:10:09 +0000

    Added.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@67 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ internals/_PDCLIB_test.h
@@ -0,0 +1,23 @@
+/* $Id$ */
+
+/* Release $Name$ */
+
+/* PDCLib testing suite <_PDCLIB_test.h>
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+/* -------------------------------------------------------------------------- */
+/* Helper macros for test drivers                                             */
+/* -------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+char const abcde[] = "abcde";
+char const abcdx[] = "abcdx";
+
+#define BEGIN_TESTS   unsigned int rc = 0
+#define TESTCASE( x ) if ( x ) {} \
+                      else { rc += 1; printf( "Testcase failed: " __FILE__ " - " #x "\n" ); }
+#define TEST_RESULTS  rc
