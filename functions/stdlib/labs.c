8ad0079c14579b76004968053abb57cc2166f245
Martin Baute <solar@rootdirectory.de>
2005-12-04 08:47:22 +0000

    Initial checkin.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@85 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/stdlib/labs.c
@@ -0,0 +1,35 @@
+/* $Id$ */
+
+/* Release $Name$ */
+
+/* labs( long int )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <stdlib.h>
+
+#ifndef REGTEST
+
+long int labs( long int j )
+{
+    return ( j >= 0 ) ? j : -j;
+}
+
+#endif
+
+#ifdef TEST
+#include <_PDCLIB_test.h>
+#include <limits.h>
+
+int main()
+{
+    BEGIN_TESTS;
+    TESTCASE( labs( 0 ) == 0 );
+    TESTCASE( labs( LONG_MAX ) == LONG_MAX );
+    TESTCASE( labs( LONG_MIN + 1 ) == -( LONG_MIN + 1 ) );
+    return TEST_RESULTS;
+}
+
+#endif
---
+++ functions/stdlib/llabs.c
@@ -0,0 +1,35 @@
+/* $Id$ */
+
+/* Release $Name$ */
+
+/* llabs( long int )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <stdlib.h>
+
+#ifndef REGTEST
+
+long long int llabs( long long int j )
+{
+    return ( j >= 0 ) ? j : -j;
+}
+
+#endif
+
+#ifdef TEST
+#include <_PDCLIB_test.h>
+#include <limits.h>
+
+int main()
+{
+    BEGIN_TESTS;
+    TESTCASE( llabs( 0 ) == 0 );
+    TESTCASE( llabs( LLONG_MAX ) == LLONG_MAX );
+    TESTCASE( llabs( LLONG_MIN + 1 ) == -( LLONG_MIN + 1 ) );
+    return TEST_RESULTS;
+}
+
+#endif
