8b634dad545a77c4326e28837a1f1adfc33906f8
Martin Baute <solar@rootdirectory.de>
2006-01-30 20:17:24 +0000

    Added test driver.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@121 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/stdlib/atexit.c
+++ functions/stdlib/atexit.c
@@ -34,11 +34,33 @@ int atexit( void (*func)( void ) )
 
 #ifdef TEST
 #include <_PDCLIB_test.h>
+#include <assert.h>
+
+static int flags[ 32 ];
+
+static void counthandler()
+{
+    static int rc = 0;
+    flags[ rc ] = rc;
+    ++rc;
+}
+
+static void checkhandler()
+{
+    for ( int i = 0; i < 31; ++i )
+    {
+        assert( flags[ i ] == i );
+    }
+}
 
 int main()
 {
     BEGIN_TESTS;
-    TESTCASE( NO_TESTDRIVER );
+    TESTCASE( atexit( &checkhandler ) == 0 );
+    for ( int i = 0; i < 31; ++i )
+    {
+        TESTCASE( atexit( &counthandler ) == 0 );
+    }
     return TEST_RESULTS;
 }
 
