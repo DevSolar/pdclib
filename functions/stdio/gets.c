b34cf7d6a449375b5ce7ec7f2677264c13b2bd50
Martin Baute <solar@rootdirectory.de>
2006-03-13 06:33:19 +0000

    Initial checkin.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@143 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/stdio/gets.c
@@ -0,0 +1,29 @@
+/* $Id$ */
+
+/* gets( char * )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <stdio.h>
+
+#ifndef REGTEST
+
+char * gets( char * s )
+{
+    return fgets( s, SIZE_MAX, stdin );
+}
+
+#endif
+
+#ifdef TEST
+#include <_PDCLIB_test.h>
+
+int main( void )
+{
+    TESTCASE( NO_TESTDRIVER );
+    return TEST_RESULTS;
+}
+
+#endif
