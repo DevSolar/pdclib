2d39a971945df678b4182513f5fb5542b1c5880c
Martin Baute <solar@rootdirectory.de>
2006-04-24 06:09:24 +0000

    Uppercase hex digits for printf() functions.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@171 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/_PDCLIB/Xdigits.c
@@ -0,0 +1,19 @@
+/* $Id$ */
+
+/* _PDCLIB_Xdigits
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+char _PDCLIB_Xdigits[] = "0123456789ABCDEF";
+
+#ifdef TEST
+
+int main( void )
+{
+    /* no tests for raw data */
+    return 0;
+}
+
+#endif
