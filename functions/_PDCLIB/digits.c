00b913e98bcb805070633e60567505b1fa1c9dc8
Martin Baute <solar@rootdirectory.de>
2006-05-24 06:13:37 +0000

    Declarating include and testdriver added.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@195 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/_PDCLIB/digits.c
+++ functions/_PDCLIB/digits.c
@@ -6,14 +6,22 @@
    Permission is granted to use, modify, and / or redistribute at will.
 */
 
+#ifndef _PDCLIB_INT_H
+#define _PDCLIB_INT_H _PDCLIB_INT_H
+#include <_PDCLIB_int.h>
+#endif
+
 char _PDCLIB_digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 
 #ifdef TEST
+#include <_PDCLIB_test.h>
+
+#include <string.h>
 
 int main( void )
 {
-    /* no tests for raw data */
-    return 0;
+    TESTCASE( strcmp( _PDCLIB_digits, "0123456789abcdefghijklmnopqrstuvwxyz" ) == 0 );
+    return TEST_RESULTS;
 }
 
 #endif
