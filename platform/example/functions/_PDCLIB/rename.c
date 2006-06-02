0a4e984363fdbcf166fe4cf02c09ffc37fcc7b5a
Martin Baute <solar@rootdirectory.de>
2006-06-02 11:59:11 +0000

    Added testdriver for _PDCLIB_rename().
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@210 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- platform/example/functions/_PDCLIB/rename.c
+++ platform/example/functions/_PDCLIB/rename.c
@@ -34,9 +34,42 @@ int _PDCLIB_rename( const char * old, const char * new )
 #undef SEEK_SET
 #include <_PDCLIB_test.h>
 
+#include <stdlib.h>
+
 int main( void )
 {
-    TESTCASE( NO_TESTDRIVER );
+    char filename1[ L_tmpnam + 6 ] = "touch ";
+    char filename2[ L_tmpnam ];
+    tmpnam( filename1 + 6 );
+    tmpnam( filename2);
+    /* check that neither file exists */
+    TESTCASE( fopen( filename1 + 6, "r" ) == NULL );
+    TESTCASE( fopen( filename2, "r" ) == NULL );
+    /* rename file 1 to file 2 - expected to fail */
+    TESTCASE( _PDCLIB_rename( filename1 + 6, filename2 ) == -1 );
+    /* create file 1 */
+    system( filename1 );
+    /* check that file 1 exists */
+    TESTCASE( fopen( filename1 + 6, "r" ) != NULL );
+    /* rename file 1 to file 2 */
+    TESTCASE( _PDCLIB_rename( filename1 + 6, filename2 ) == 0 );
+    /* check that file 2 exists, file 1 does not */
+    TESTCASE( fopen( filename1 + 6, "r" ) == NULL );
+    TESTCASE( fopen( filename2, "r" ) != NULL );
+    /* create another file 1 */
+    system( filename1 );
+    /* check that file 1 exists */
+    TESTCASE( fopen( filename1 + 6, "r" ) != NULL );
+    /* rename file 1 to file 2 - expected to fail, see comment in
+       _PDCLIB_rename() itself.
+    */
+    TESTCASE( _PDCLIB_rename( filename1 + 6, filename2 ) == -1 );
+    /* remove both files */
+    remove( filename1 + 6 );
+    remove( filename2 );
+    /* check that they're gone */
+    TESTCASE( fopen( filename1 + 6, "r" ) == NULL );
+    TESTCASE( fopen( filename2, "r" ) == NULL );
     return TEST_RESULTS;
 }
 
