dde133700cf400233a59855a3545e2fef521994c
Martin Baute <solar@rootdirectory.de>
2006-06-07 14:16:45 +0000

    Added close().
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@214 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ platform/example/functions/_PDCLIB/close.c
@@ -0,0 +1,39 @@
+/* $Id$ */
+
+/* _PDCLIB_close( _PDCLIB_fd_t fd )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+/* This is an example implementation of _PDCLIB_close() fit for use with POSIX
+   kernels.
+*/
+
+#include <stdio.h>
+
+#ifndef REGTEST
+#include <_PDCLIB_glue.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _PDCLIB_close( _PDCLIB_fd_t fd )
+{
+    return close( fd );
+}
+
+#endif
+
+#ifdef TEST
+#include <_PDCLIB_test.h>
+
+int main( void )
+{
+    /* No testdriver; tested in driver for _PDCLIB_open(). */
+    return TEST_RESULTS;
+}
+
+#endif
