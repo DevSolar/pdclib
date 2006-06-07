2609f5d25d2d3d370f5d1f19ce20a8f79a8387fc
Martin Baute <solar@rootdirectory.de>
2006-06-07 05:36:39 +0000

    First working testdriver.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@212 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- platform/example/functions/_PDCLIB/open.c
+++ platform/example/functions/_PDCLIB/open.c
@@ -22,13 +22,45 @@
 
 _PDCLIB_fd_t _PDCLIB_open( char const * const filename, unsigned int mode )
 {
-    int osmode = 0;
-    if ( mode & _PDCLIB_FRW ) osmode |= O_RDWR;
-    if ( mode & ( _PDCLIB_FWRITE | _PDCLIB_FAPPEND ) ) osmode |= O_CREAT;
-    if ( mode & _PDCLIB_FWRITE ) osmode |= O_TRUNC;
-    if ( mode & _PDCLIB_FAPPEND ) osmode |= O_APPEND;
-    if ( ( mode & _PDCLIB_FREAD ) && ! ( mode & _PDCLIB_FRW ) ) osmode |= O_RDONLY;
-    return open( filename, osmode );
+    /* FIXME: THIS IS NOT TO BE USED OUT-OF-THE-BOX.
+       It is a proof-of-concept implementation. E.g. a stream may only be fully
+       buffered IF IT CAN BE DETERMINED NOT TO REFER TO AN INTERACTIVE DEVICE.
+       This logic is not represented here, as this is the EXAMPLE platform, and
+       actual platform overlays may differ widely. Another point is the value
+       for permissions being hardcoded to 0664 for file creations.
+    */
+    int osmode;
+    switch ( mode & ~_PDCLIB_FBIN )
+    {
+        case _PDCLIB_FREAD: /* "r" */
+            osmode = O_RDONLY;
+            break;
+        case _PDCLIB_FWRITE: /* "w" */
+            osmode = O_WRONLY | O_CREAT | O_TRUNC;
+            break;
+        case _PDCLIB_FAPPEND: /* "a" */
+            osmode = O_APPEND | O_CREAT;
+            break;
+        case _PDCLIB_FREAD | _PDCLIB_FRW: /* "r+" */
+            osmode = O_RDWR;
+            break;
+        case _PDCLIB_FWRITE | _PDCLIB_FRW: /* "w+" */
+            osmode = O_RDWR | O_CREAT | O_TRUNC;
+            break;
+        case _PDCLIB_FAPPEND | _PDCLIB_FRW: /* "a+" */
+            osmode = O_RDWR | O_APPEND | O_CREAT;
+            break;
+        default: /* Invalid mode */
+            return -1;
+    }
+    if ( osmode & O_CREAT )
+    {
+        return open( filename, osmode, S_IRUSR | S_IWUSR );
+    }
+    else
+    {
+        return open( filename, osmode );
+    }
 }
 
 #endif
@@ -36,9 +68,20 @@ _PDCLIB_fd_t _PDCLIB_open( char const * const filename, unsigned int mode )
 #ifdef TEST
 #include <_PDCLIB_test.h>
 
+#include <stdlib.h>
+
 int main( void )
 {
-    TESTCASE( NO_TESTDRIVER );
+    /* This testdriver assumes POSIX, i.e. _PDCLIB_fd_t being int and being
+       incremented by one on each successful open.
+    */
+    _PDCLIB_fd_t fh;
+    TESTCASE( _PDCLIB_open( "testfile2", _PDCLIB_FREAD ) == _PDCLIB_NOHANDLE );
+    TESTCASE( ( fh = _PDCLIB_open( "testfile1", _PDCLIB_FWRITE ) ) != _PDCLIB_NOHANDLE );
+    TESTCASE( write( fh, "test", 4 ) == 4 );
+    TESTCASE( close( fh ) == 0 );
+    TESTCASE( _PDCLIB_open( "testfile1", _PDCLIB_FREAD ) != _PDCLIB_NOHANDLE );
+    system( "rm testfile1" );
     return TEST_RESULTS;
 }
 
