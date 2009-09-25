5f9c5cafb744ed476a8a1bd778cd21636c80fb10
Martin Baute <solar@rootdirectory.de>
2009-09-25 05:52:28 +0000

    Started debugging scanf() functions.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@265 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/stdio/vfscanf.c
+++ functions/stdio/vfscanf.c
@@ -8,13 +8,63 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <ctype.h>
 
 #ifndef REGTEST
 
 int vfscanf( FILE * _PDCLIB_restrict stream, const char * _PDCLIB_restrict format, va_list arg )
 {
-    /* TODO: Implement using vsscanf() reading from file buffer */
-    return 0;
+    struct _PDCLIB_status_t status;
+    status.base = 0;
+    status.flags = 0;
+    status.n = 0; 
+    status.i = 0;
+    status.this = 0;
+    status.s = NULL;
+    status.width = 0;
+    status.prec = 0;
+    status.stream = stream;
+    va_copy( status.arg, arg );
+    while ( *format != '\0' )
+    {
+        const char * rc;
+        if ( ( *format != '%' ) || ( ( rc = _PDCLIB_scan( format, &status ) ) == format ) )
+        {
+            /* No conversion specifier, match verbatim */
+            if ( isspace( *format ) )
+            {
+                /* Whitespace char in format string: Skip all whitespaces */
+                /* No whitespaces in input do not result in matching error */
+                while ( isspace( *status.s ) )
+                {
+                    ++status.s;
+                    ++status.i;
+                }
+            }
+            else
+            {
+                /* Non-whitespace char in format string: Match verbatim */
+                if ( *status.s != *format )
+                {
+                    /* Matching error */
+                    return status.n;
+                }
+                else
+                {
+                    ++status.s;
+                    ++status.i;
+                }
+            }
+            ++format;
+        }
+        else
+        {
+            /* Continue parsing after conversion specifier */
+            format = rc;
+        }
+    }
+    va_end( status.arg );
+    return status.n;
 }
 
 #endif
