c1bd43fa7b2a2c0546f61377acf246243a43726e
Martin Baute <solar@rootdirectory.de>
2010-12-20 07:15:23 +0000

    Basic tests for %c, %s, %[ and %p.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@380 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- testing/scanf_testcases.h
+++ testing/scanf_testcases.h
@@ -2,6 +2,7 @@
     char buffer[100];
     int i;
     unsigned int u;
+    int * p;
     /* basic: reading of three-char string */
     SCANF_TEST( 1, "foo", "%3c", buffer );
     TESTCASE( memcmp( buffer, "foo", 3 ) == 0 );
@@ -56,4 +57,31 @@
     TESTCASE( u == 0 );
     SCANF_TEST( 1, INT_OCTDIG, "%o", &u );
     TESTCASE( u == UINT_MAX );
+    /* testing %c */
+    memset( buffer, '\0', 100 );
+    SCANF_TEST( 1, "x", "%c", buffer );
+    TESTCASE( memcmp( buffer, "x\0", 2 ) == 0 );
+    /* testing %s */
+    memset( buffer, '\0', 100 );
+    SCANF_TEST( 1, "foo bar", "%s", buffer );
+    TESTCASE( memcmp( buffer, "foo\0", 4 ) == 0 );
+#ifndef TEST_CONVERSION_ONLY
+    SCANF_TEST( 2, "foo bar  baz", "%s %s %n", buffer, buffer + 4, &u );
+    TESTCASE( u == 9 );
+    TESTCASE( memcmp( buffer, "foo\0bar\0", 8 ) == 0 );
+#endif
+    /* testing %[ */
+    SCANF_TEST( 1, "abcdefg", "%[cba]", buffer );
+    TESTCASE( memcmp( buffer, "abc\0", 4 ) == 0 );
+    /* testing %p */
+    p = NULL;
+    sprintf( buffer, "%p", p );
+    p = &i;
+    SCANF_TEST( 1, buffer, "%p", &p );
+    TESTCASE( p == NULL );
+    p = &i;
+    sprintf( buffer, "%p", p );
+    p = NULL;
+    SCANF_TEST( 1, buffer, "%p", &p );
+    TESTCASE( p == &i );
 }
