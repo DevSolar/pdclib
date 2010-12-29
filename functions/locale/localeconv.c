a64d83c3f57cd9eef3625521ee30480bc043f421
Martin Baute <solar@rootdirectory.de>
2010-12-29 13:31:51 +0000

    localeconv, and setlocale (C locale only)
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@386 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/locale/localeconv.c
@@ -0,0 +1,28 @@
+/* $Id$ */
+
+/* localeconv( void )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <locale.h>
+
+#ifndef REGTEST
+
+struct lconv * localeconv( void )
+{
+    return &_PDCLIB_lconv;
+}
+
+#endif
+
+#ifdef TEST
+#include <_PDCLIB_test.h>
+
+int main( void )
+{
+    TESTCASE( NO_TESTDRIVER );
+    return TEST_RESULTS;
+}
+#endif
