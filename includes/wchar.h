d60730caed556a49d007126341c7ddfbfe48c824
Erin Shepherd <erin.shepherd@e43.eu>
2012-08-29 23:29:27 +0000

    PDCLIB-2: Complete wchar.h + manual page. This header is a monster, implies numerous backend changes, and therefore is going to be a bit of a slog :-(
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/branches/shepherd@464 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- includes/wchar.h
+++ includes/wchar.h
@@ -21,15 +21,106 @@ typedef _PDCLIB_size_t size_t;
 #ifndef __cplusplus
 #ifndef _PDCLIB_WCHAR_T_DEFINED
 #define _PDCLIB_WCHAR_T_DEFINED _PDCLIB_WCHAR_T_DEFINED
-typedef _PDCLIB_wchar_t   wchar_t;
+typedef _PDCLIB_wchar_t wchar_t;
 #endif
 #endif
 
-wchar_t *wcschr( const wchar_t * haystack, wchar_t needle ) _PDCLIB_nothrow;
-wchar_t *wcsrchr( const wchar_t * haystack, wchar_t needle ) _PDCLIB_nothrow;
-size_t   wcslen( const wchar_t * string) _PDCLIB_nothrow;
-wchar_t *wcscpy( wchar_t * _PDCLIB_restrict dest, 
-                 const wchar_t * _PDCLIB_restrict src) _PDCLIB_nothrow;
+#ifndef _PDCLIB_WINT_T_DEFINED
+#define _PDCLIB_WINT_T_DEFINED _PDCLIB_WINT_T_DEFINED
+typedef _PDCLIB_wint_t wint_t;
+#endif
+
+#ifndef _PDCLIB_MBSTATE_T_DEFINED
+#define _PDCLIB_MBSTATE_T_DEFINED _PDCLIB_MBSTATE_T_DEFINED
+typedef _PDCLIB_mbstate_t mbstate_t;
+#endif
+
+struct tm;
+
+#ifndef _PDCLIB_NULL_DEFINED
+#define _PDCLIB_NULL_DEFINED _PDCLIB_NULL_DEFINED
+#define NULL _PDCLIB_NULL
+#endif
+
+#ifndef _PDCLIB_WCHAR_MIN_MAX_DEFINED
+#define _PDCLIB_WCHAR_MIN_MAX_DEFINED
+#define WCHAR_MIN _PDCLIB_WCHAR_MIN
+#define WCHAR_MAX _PDCLIB_WCHAR_MAX
+#endif
+
+#define WEOF ((wint_t) -1)
+
+/* Wide character string handling */
+wchar_t *wcscpy(wchar_t *_PDCLIB_restrict s1, const wchar_t *_PDCLIB_restrict s2);
+wchar_t *wcsncpy(wchar_t *_PDCLIB_restrict s1, const wchar_t *_PDCLIB_restrict s2, size_t n);
+wchar_t *wmemcpy(wchar_t *_PDCLIB_restrict s1, const wchar_t *_PDCLIB_restrict s2, size_t n);
+wchar_t *wmemmove(wchar_t *s1, const wchar_t *s2, size_t n);
+wchar_t *wcscat(wchar_t *_PDCLIB_restrict s1, const wchar_t *_PDCLIB_restrict s2);
+wchar_t *wcsncat(wchar_t *_PDCLIB_restrict s1, const wchar_t *_PDCLIB_restrict s2, size_t n);
+int wcscmp(const wchar_t *s1, const wchar_t *s2);
+int wcscoll(const wchar_t *s1, const wchar_t *s2);
+int wcsncmp(const wchar_t *s1, const wchar_t *s2, size_t n);
+size_t wcsxfrm(wchar_t *_PDCLIB_restrict s1, const wchar_t *_PDCLIB_restrict s2, size_t n);
+int wmemcmp(const wchar_t *s1, const wchar_t *s2, size_t n);
+wchar_t *wcschr(const wchar_t *s, wchar_t c);
+size_t wcscspn(const wchar_t *s1, const wchar_t *s2);
+wchar_t *wcspbrk(const wchar_t *s1, const wchar_t *s2);
+wchar_t *wcsrchr(const wchar_t *s, wchar_t c);
+size_t wcsspn(const wchar_t *s1, const wchar_t *s2);
+wchar_t *wcsstr(const wchar_t *s1, const wchar_t *s2);
+wchar_t *wcstok(wchar_t *_PDCLIB_restrict s1, const wchar_t *_PDCLIB_restrict s2, wchar_t **_PDCLIB_restrict ptr);
+wchar_t *wmemchr(const wchar_t *s, wchar_t c, size_t n); 
+size_t wcslen(const wchar_t *s);
+wchar_t *wmemset(wchar_t *s, wchar_t c, size_t n);
+
+#if 0
+size_t wcsftime(wchar_t *_PDCLIB_restrict s, size_t maxsize, const wchar_t *_PDCLIB_restrict format, const struct tm *_PDCLIB_restrict timeptr);
+#endif
+
+/* Wide character I/O */
+int fwprintf(FILE *_PDCLIB_restrict stream, const wchar_t *_PDCLIB_restrict format, ...);
+int fwscanf(FILE *_PDCLIB_restrict stream, const wchar_t *_PDCLIB_restrict format, ...);
+int swprintf(wchar_t *_PDCLIB_restrict s, size_t n, const wchar_t *_PDCLIB_restrict format, ...);
+int swscanf(const wchar_t *_PDCLIB_restrict s, const wchar_t *_PDCLIB_restrict format, ...);
+int vfwprintf(FILE *_PDCLIB_restrict stream, const wchar_t *_PDCLIB_restrict format, va_list arg);
+int vfwscanf(FILE *_PDCLIB_restrict stream, const wchar_t *_PDCLIB_restrict format, va_list arg);
+int vswprintf(wchar_t *_PDCLIB_restrict s, size_t n, const wchar_t *_PDCLIB_restrict format, va_list arg);
+int vswscanf(const wchar_t *_PDCLIB_restrict s, const wchar_t *_PDCLIB_restrict format, va_list arg);
+int vwprintf(const wchar_t *_PDCLIB_restrict format, va_list arg);
+int vwscanf(const wchar_t *_PDCLIB_restrict format, va_list arg);
+int wprintf(const wchar_t *_PDCLIB_restrict format, ...);
+int wscanf(const wchar_t *_PDCLIB_restrict format, ...);
+wint_t fgetwc(FILE *stream);
+wchar_t *fgetws(wchar_t *_PDCLIB_restrict s, int n, FILE *_PDCLIB_restrict stream);
+wint_t fputwc(wchar_t c, FILE *stream);
+int fputws(const wchar_t *_PDCLIB_restrict s, FILE *_PDCLIB_restrict stream);
+int fwide(FILE *stream, int mode);
+wint_t getwc(FILE *stream);
+wint_t getwchar(void);
+wint_t putwc(wchar_t c, FILE *stream);
+wint_t putwchar(wchar_t c);
+wint_t ungetwc(wint_t c, FILE *stream);
+
+/* Wide character <-> Numeric conversions */
+#if 0
+double wcstod(const wchar_t *_PDCLIB_restrict nptr, wchar_t **_PDCLIB_restrict endptr);
+float wcstof(const wchar_t *_PDCLIB_restrict nptr, wchar_t **_PDCLIB_restrict endptr);
+long double wcstold(const wchar_t *_PDCLIB_restrict nptr, wchar_t **_PDCLIB_restrict endptr);
+#endif
+long int wcstol(const wchar_t *_PDCLIB_restrict nptr, wchar_t **_PDCLIB_restrict endptr, int base);
+long long int wcstoll(const wchar_t *_PDCLIB_restrict nptr, wchar_t **_PDCLIB_restrict endptr, int base);
+unsigned long int wcstoul(const wchar_t *_PDCLIB_restrict nptr, wchar_t **_PDCLIB_restrict endptr, int base);
+unsigned long long int wcstoull(const wchar_t *_PDCLIB_restrict nptr, wchar_t **_PDCLIB_restrict endptr, int base);
+
+/* Character set conversion */
+wint_t btowc(int c);
+int wctob(wint_t c);
+int mbsinit(const mbstate_t *ps);
+size_t mbrlen(const char *_PDCLIB_restrict s, size_t n, mbstate_t *_PDCLIB_restrict ps);
+size_t mbrtowc(wchar_t *_PDCLIB_restrict pwc, const char *_PDCLIB_restrict s, size_t n, mbstate_t *_PDCLIB_restrict ps);
+size_t wcrtomb(char *_PDCLIB_restrict s, wchar_t wc, mbstate_t *_PDCLIB_restrict ps);
+size_t mbsrtowcs(wchar_t *_PDCLIB_restrict dst, const char **_PDCLIB_restrict src, size_t len, mbstate_t *_PDCLIB_restrict ps);
+size_t wcsrtombs(char *_PDCLIB_restrict dst, const wchar_t **_PDCLIB_restrict src, size_t len, mbstate_t *_PDCLIB_restrict ps);
 
 _PDCLIB_END_EXTERN_C
 #endif
