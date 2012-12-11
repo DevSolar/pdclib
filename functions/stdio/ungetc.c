d867151620867c2879c65658f05f80bcb2948cc9
Erin Shepherd <erin.shepherd@e43.eu>
2012-12-11 00:55:36 +0000

    PDCLIB-18: Implement defect test cases
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/branches/shepherd@480 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/stdio/ungetc.c
+++ functions/stdio/ungetc.c
@@ -32,9 +32,35 @@ int ungetc( int c, struct _PDCLIB_file_t * stream )
 #ifdef TEST
 #include <_PDCLIB_test.h>
 
+const char* hellostr = "Hello, world!";
+
 int main( void )
 {
-    /* Testing covered by ftell.c */
+    // Also see ftell() for some testing
+
+    // PDCLIB-18: fread ignores ungetc
+    size_t bufsz = strlen( hellostr ) + 1;
+    char * buf = malloc( bufsz );
+    FILE * fh;
+
+    // Also fgets
+    TESTCASE( ( fh = tmpfile() ) != NULL );
+    TESTCASE( fputs(hellostr, fh) == 0 );
+    rewind(fh);
+    TESTCASE( fgetc( fh ) == 'H' );
+    TESTCASE( ungetc( 'H', fh ) == 'H' );
+    TESTCASE( fgets( buf, bufsz, fh ) != NULL );
+    TESTCASE( strcmp( buf, hellostr ) == 0 );
+
+    // fread
+    rewind(fh);
+    TESTCASE( fgetc( fh ) == 'H' );
+    TESTCASE( ungetc( 'H', fh ) == 'H' );
+    TESTCASE( fread( buf, bufsz - 1, 1, fh ) == 1 );
+    TESTCASE( strncmp( buf, hellostr, bufsz - 1 ) == 0 );
+
+
+
     return TEST_RESULTS;
 }
 
