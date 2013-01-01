f5cd8c61b84927bf3a94acca72cd25d672d44c7e
Erin Shepherd <erin.shepherd@e43.eu>
2013-01-01 21:09:49 +0000

    PDCLIB-2 PDCLIB-9 wcrtomb
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/branches/shepherd@543 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/wchar/wcrtomb.c
@@ -0,0 +1,48 @@
+/* wcrtomb(
+    char        *restrict   s, 
+    wchar_t                 wc,
+    mbstate_t   *restrict   ps);
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#ifndef REGTEST
+#include <wchar.h>
+#include <errno.h>
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <_PDCLIB_encoding.h>
+#include <_PDCLIB_locale.h>
+
+size_t wcrtomb_l(
+    char        *restrict   s, 
+    wchar_t                 wc,
+    mbstate_t   *restrict   ps,
+    locale_t     restrict   l
+)
+{
+    return _PDCLIB_cwcrtomb_l(s, wc, ps, l);
+}
+
+size_t wcrtomb(
+    char        *restrict   s, 
+    wchar_t                 wc,
+    mbstate_t   *restrict   ps
+)
+{
+    return _PDCLIB_cwcrtomb(s, wc, ps);
+}
+
+#endif
+
+#ifdef TEST
+#include <_PDCLIB_test.h>
+
+int main( void )
+{
+    TESTCASE( NO_TESTDRIVER );
+    return TEST_RESULTS;
+}
+#endif
