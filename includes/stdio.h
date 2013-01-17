cc861a9ee258e0f39b664f073a2bf9d2b7ef6930
Erin Shepherd <erin.shepherd@e43.eu>
2013-01-17 15:50:27 +0000

    Annotate _unlocked stdio as nothrow
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/branches/shepherd@558 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- includes/stdio.h
+++ includes/stdio.h
@@ -840,30 +840,30 @@ void perror( const char * s ) _PDCLIB_nothrow;
  * stream isn't locked by the calling thread is implementation defined.
  */
 #if _PDCLIB_POSIX_MIN(200112L) || _PDCLIB_BSD_SOURCE || _PDCLIB_SVID_SOURCE
-void flockfile(FILE *file);
-int ftrylockfile(FILE *file);
-void funlockfile(FILE *file);
+void flockfile(FILE *file) _PDCLIB_nothrow;
+int ftrylockfile(FILE *file) _PDCLIB_nothrow;
+void funlockfile(FILE *file) _PDCLIB_nothrow;
 
-int getc_unlocked(FILE *stream);
-int getchar_unlocked(void);
-int putc_unlocked(int c, FILE *stream);
-int putchar_unlocked(int c);
+int getc_unlocked(FILE *stream) _PDCLIB_nothrow;
+int getchar_unlocked(void) _PDCLIB_nothrow;
+int putc_unlocked(int c, FILE *stream) _PDCLIB_nothrow;
+int putchar_unlocked(int c) _PDCLIB_nothrow;
 #endif
 
 #if _PDCLIB_BSD_SOURCE || _PDCLIB_SVID_SOURCE
-void clearerr_unlocked(FILE *stream); 
-int feof_unlocked(FILE *stream);
-int ferror_unlocked(FILE *stream);
-int fflush_unlocked(FILE *stream);
-int fgetc_unlocked(FILE *stream);
-int fputc_unlocked(int c, FILE *stream);
-size_t fread_unlocked(void *ptr, size_t size, size_t n, FILE *stream);
-size_t fwrite_unlocked(const void *ptr, size_t size, size_t n, FILE *stream);
+void clearerr_unlocked(FILE *stream) _PDCLIB_nothrow; 
+int feof_unlocked(FILE *stream) _PDCLIB_nothrow;
+int ferror_unlocked(FILE *stream) _PDCLIB_nothrow;
+int fflush_unlocked(FILE *stream) _PDCLIB_nothrow;
+int fgetc_unlocked(FILE *stream) _PDCLIB_nothrow;
+int fputc_unlocked(int c, FILE *stream) _PDCLIB_nothrow;
+size_t fread_unlocked(void *ptr, size_t size, size_t n, FILE *stream) _PDCLIB_nothrow;
+size_t fwrite_unlocked(const void *ptr, size_t size, size_t n, FILE *stream) _PDCLIB_nothrow;
 #endif
 
 #if _PDCLIB_GNU_SOURCE
-char *fgets_unlocked(char *s, int n, FILE *stream);
-int fputs_unlocked(const char *s, FILE *stream);
+char *fgets_unlocked(char *s, int n, FILE *stream) _PDCLIB_nothrow;
+int fputs_unlocked(const char *s, FILE *stream) _PDCLIB_nothrow;
 #endif
 
 #if defined(_PDCLIB_EXTENSIONS)
