6c43febaa91e59babf908568ee4ebe6731e1a05b
Erin Shepherd <erin.shepherd@e43.eu>
2014-11-12 20:37:29 +0000

    fwrite: output line buffered content as we go along
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/branches/shepherd@614 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/stdio/fwrite.c
+++ functions/stdio/fwrite.c
@@ -15,83 +15,48 @@
 #include <stdbool.h>
 #include <string.h>
 
-//TODO OS(2012-08-01): Ascertain purpose of lineend & potentially remove
-
-size_t _PDCLIB_fwrite_unlocked( const void * _PDCLIB_restrict ptr, 
-               size_t size, size_t nmemb, 
+size_t _PDCLIB_fwrite_unlocked( const void *restrict vptr,
+               size_t size, size_t nmemb,
                FILE * _PDCLIB_restrict stream )
 {
     if ( _PDCLIB_prepwrite( stream ) == EOF )
     {
         return 0;
     }
-    _PDCLIB_size_t offset = 0;
-    //bool lineend = false;
+
+    const char *restrict ptr = vptr;
     size_t nmemb_i;
     for ( nmemb_i = 0; nmemb_i < nmemb; ++nmemb_i )
     {
         for ( size_t size_i = 0; size_i < size; ++size_i )
         {
-            if ( ( stream->buffer[ stream->bufidx++ ] = ((char*)ptr)[ nmemb_i * size + size_i ] ) == '\n' )
-            {
-                /* Remember last newline, in case we have to do a partial line-buffered flush */
-                offset = stream->bufidx;
-                //lineend = true;
-            }
-            if ( stream->bufidx == stream->bufsize )
+            char c = ptr[ nmemb_i * size + size_i ];
+            stream->buffer[ stream->bufidx++ ] = c;
+
+            if ( stream->bufidx == stream->bufsize || ( c == '\n' && stream->status & _IOLBF ) )
             {
                 if ( _PDCLIB_flushbuffer( stream ) == EOF )
                 {
                     /* Returning number of objects completely buffered */
                     return nmemb_i;
                 }
-                //lineend = false;
-                /*
-                 * The entire buffer has been flushed; this means we have to
-                 * reset our newline position as we have already written
-                 * that part of the stream.
-                 */
-                offset = 0;
             }
         }
-    }
-    /* Fully-buffered streams are OK. Non-buffered streams must be flushed,
-       line-buffered streams only if there's a newline in the buffer.
-    */
-    switch ( stream->status & ( _IONBF | _IOLBF ) )
-    {
-        case _IONBF:
+
+        if ( stream->status & _IONBF )
+        {
             if ( _PDCLIB_flushbuffer( stream ) == EOF )
             {
-                /* We are in a pinch here. We have an error, which requires a
-                   return value < nmemb. On the other hand, all objects have
-                   been written to buffer, which means all the caller had to
-                   do was removing the error cause, and re-flush the stream...
-                   Catch 22. We'll return a value one short, to indicate the
-                   error, and can't really do anything about the inconsistency.
-                */
-                return nmemb_i - 1;
+                /* Returning number of objects completely buffered */
+                return nmemb_i;
             }
-            break;
-        case _IOLBF:
-            {
-            size_t bufidx = stream->bufidx;
-            stream->bufidx = offset;
-            if ( _PDCLIB_flushbuffer( stream ) == EOF )
-            {
-                /* See comment above. */
-                stream->bufidx = bufidx;
-                return nmemb_i - 1;
-            }
-            stream->bufidx = bufidx - offset;
-            memmove( stream->buffer, stream->buffer + offset, stream->bufidx );
-            }
+        }
     }
     return nmemb_i;
 }
 
-size_t fwrite( const void * _PDCLIB_restrict ptr, 
-               size_t size, size_t nmemb, 
+size_t fwrite( const void * _PDCLIB_restrict ptr,
+               size_t size, size_t nmemb,
                FILE * _PDCLIB_restrict stream )
 {
     _PDCLIB_flockfile( stream );
