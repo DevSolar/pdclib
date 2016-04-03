4f1ba4e53779dcdef9158a30a595a19d2c0cd243
Martin Baute <solar@rootdirectory.de>
2016-04-03 07:05:13 +0000

    POSIX interfacing time retrieval
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@690 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ platform/example/functions/time/time.c
@@ -0,0 +1,41 @@
+/* time( time_t * )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <time.h>
+
+#ifndef REGTEST
+
+#include <sys/time.h>
+
+/* See comments in time.h on the semantics of time_t. */
+
+time_t time( time_t * timer )
+{
+    struct timeval tv;
+    if ( gettimeofday( &tv, NULL ) == 0 )
+    {
+        if ( timer != NULL )
+        {
+            *timer = tv.tv_sec;
+        }
+        return tv.tv_sec;
+    }
+    return -1;
+}
+
+#endif
+
+#ifdef TEST
+
+#include "_PDCLIB_test.h"
+
+int main( void )
+{
+    TESTCASE( NO_TESTDRIVER );
+    return TEST_RESULTS;
+}
+
+#endif
---
+++ platform/example/functions/time/timespec_get.c
@@ -0,0 +1,42 @@
+/* timespec_get( struct timespec *, int )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include <time.h>
+
+#ifndef REGTEST
+
+#include <sys/time.h>
+
+int timespec_get( struct timespec * ts, int base )
+{
+    if ( base == TIME_UTC )
+    {
+        /* We can make do with a really thin wrapper here. */
+        struct timeval tv;
+        if ( gettimeofday( &tv, NULL ) == 0 )
+        {
+            ts->tv_sec = tv.tv_sec;
+            ts->tv_nsec = tv.tv_usec * 1000;
+            return base;
+        }
+    }
+    /* Not supporting any other time base than TIME_UTC for now. */
+    return 0;
+}
+
+#endif
+
+#ifdef TEST
+
+#include "_PDCLIB_test.h"
+
+int main( void )
+{
+    TESTCASE( NO_TESTDRIVER );
+    return TEST_RESULTS;
+}
+
+#endif
