7efa29574753743454a8ab4e403ef36b9263a516
Martin Baute <solar@rootdirectory.de>
2018-06-18 12:23:59 +0000

    Ugly bug in strncmp: The strings do not need to be null-terminated, so accessing *s1 before checking n can invoke UB. Fixed.
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@721 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/string/strncpy.c
+++ functions/string/strncpy.c
@@ -11,7 +11,7 @@
 char * strncpy( char * _PDCLIB_restrict s1, const char * _PDCLIB_restrict s2, size_t n )
 {
     char * rc = s1;
-    while ( ( n > 0 ) && ( *s1++ = *s2++ ) )
+    while ( n && ( *s1++ = *s2++ ) )
     {
         /* Cannot do "n--" in the conditional as size_t is unsigned and we have
            to check it again for >0 in the next loop below, so we must not risk
