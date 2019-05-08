8bfe8a43b0bc84604c5461e9f3337a4a4212108f
Martin Baute <solar@rootdirectory.de>
2019-05-08 09:48:15 +0000

    ignore_handler_s
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@804 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/stdlib/ignore_handler_s.c
@@ -0,0 +1,31 @@
+/* ignore_handler_s( const char *, void *, errno_t )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdlib.h>
+
+#ifndef REGTEST
+
+#include <stdio.h>
+
+void ignore_handler_s( const char * _PDCLIB_restrict msg, void * _PDCLIB_restrict ptr, errno_t errno )
+{
+    return;
+}
+
+#endif
+
+#ifdef TEST
+
+#include "_PDCLIB_test.h"
+
+int main( void )
+{
+    TESTCASE( NO_TESTDRIVER );
+    return TEST_RESULTS;
+}
+
+#endif
