45ba1157c8a17a32899fddf84bf669ef7e5ba1e2
Martin Baute <solar@rootdirectory.de>
2019-05-09 09:44:31 +0000

    Simplified constraint handler test.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@805 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/stdlib/set_constraint_handler_s.c
+++ functions/stdlib/set_constraint_handler_s.c
@@ -33,20 +33,13 @@ constraint_handler_t set_constraint_handler_s( constraint_handler_t handler )
 
 #include "_PDCLIB_test.h"
 
-#ifndef REGTEST
-static void new_constraint_handler( const char * _PDCLIB_restrict msg, void * _PDCLIB_restrict ptr, errno_t errno )
-{
-    abort();
-}
-#endif
-
 int main( void )
 {
 #ifndef REGTEST
     TESTCASE( _PDCLIB_constraint_handler == abort_handler_s );
-    TESTCASE( set_constraint_handler_s( new_constraint_handler ) == abort_handler_s );
-    TESTCASE( _PDCLIB_constraint_handler == new_constraint_handler );
-    TESTCASE( set_constraint_handler_s( NULL ) == new_constraint_handler );
+    TESTCASE( set_constraint_handler_s( ignore_handler_s ) == abort_handler_s );
+    TESTCASE( _PDCLIB_constraint_handler == ignore_handler_s );
+    TESTCASE( set_constraint_handler_s( NULL ) == ignore_handler_s );
     TESTCASE( _PDCLIB_constraint_handler == abort_handler_s );
 #endif
     return TEST_RESULTS;
