4c64737ae94c695e163fb99437bf43dca3985eeb
solar <solar@bcf39385-58cc-4174-9fcf-14f50f90dd47>
2019-06-12 12:58:08 +0000

    memcpy_s
    git-svn-id: svn://rootdirectory.ddns.net/pdclib/trunk@823 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/string/memcpy_s.c
+++ functions/string/memcpy_s.c
@@ -6,6 +6,8 @@
 
 #define __STDC_WANT_LIB_EXT1__ 1
 #include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 #ifndef REGTEST
 
@@ -13,10 +15,22 @@ errno_t memcpy_s( void * _PDCLIB_restrict s1, rsize_t s1max, const void * _PDCLI
 {
     char * dest = (char *) s1;
     const char * src = (const char *) s2;
+
+    if ( s1 == NULL || s2 == NULL || s1max > RSIZE_MAX || n > RSIZE_MAX || n > s1max || ( dest < src && ( dest + n ) >= src ) || ( src < dest && ( src + n ) >= dest ) || ( n > 0 && src == dest ) )
+    {
+        if ( s1 != NULL && s1max <= RSIZE_MAX )
+        {
+            memset( s1, 0, s1max );
+        }
+        _PDCLIB_constraint_handler( _PDCLIB_CONSTRAINT_VIOLATION( _PDCLIB_EINVAL ) );
+        return _PDCLIB_EINVAL;
+    }
+
     while ( n-- )
     {
         *dest++ = *src++;
     }
+
     return 0;
 }
 
@@ -28,13 +42,24 @@ errno_t memcpy_s( void * _PDCLIB_restrict s1, rsize_t s1max, const void * _PDCLI
 
 int main( void )
 {
+#ifndef REGTEST
     char s[] = "xxxxxxxxxxx";
-    TESTCASE( memcpy( s, abcde, 6 ) == s );
+    TESTCASE( memcpy_s( s, 11, abcde, 6 ) == 0 );
     TESTCASE( s[4] == 'e' );
     TESTCASE( s[5] == '\0' );
-    TESTCASE( memcpy( s + 5, abcde, 5 ) == s + 5 );
+    TESTCASE( memcpy_s( s + 5, 6, abcde, 5 ) == 0 );
     TESTCASE( s[9] == 'e' );
     TESTCASE( s[10] == 'x' );
+
+    set_constraint_handler_s( ignore_handler_s );
+    s[5] = 'x';
+
+    TESTCASE( memcpy_s( s, 5, abcde, 6 ) != 0 );
+    TESTCASE( memcmp( s, "\0\0\0\0\0x", 6 ) == 0 );
+
+    TESTCASE( memcpy_s( s, 3, s + 2, 3 ) != 0 );
+    TESTCASE( memcpy_s( s + 2, 3, s, 3 ) != 0 );
+#endif
     return TEST_RESULTS;
 }
 
