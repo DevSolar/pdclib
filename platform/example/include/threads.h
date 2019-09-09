745bab605976bf699e8adfbc531b8628ff550510
Martin Baute <solar@rootdirectory.de>
2019-09-09 14:11:45 +0000

    _PDCLIB_PUBLIC for platform includes.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@843 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- platform/example/include/threads.h
+++ platform/example/include/threads.h
@@ -78,7 +78,7 @@ enum
 /* Ensure that func is called only the first time call_once() is called
    for a given flag.
 */
-void call_once( once_flag * flag, void (*func)( void ) );
+_PDCLIB_PUBLIC void call_once( once_flag * flag, void (*func)( void ) );
 
 /* Condition variable functions */
 
@@ -86,12 +86,12 @@ void call_once( once_flag * flag, void (*func)( void ) );
    Returns thrd_success if successful, thrd_error if the request could not
    be honored.
 */
-int cnd_broadcast( cnd_t * cond );
+_PDCLIB_PUBLIC int cnd_broadcast( cnd_t * cond );
 
 /* Destroy condition variable.
    No threads may be waiting on a condition when it is destroyed.
 */
-void cnd_destroy( cnd_t * cond );
+_PDCLIB_PUBLIC void cnd_destroy( cnd_t * cond );
 
 /* Initialize condition variable.
    Returns thrd_success if successful, thrd_nomem if out of memory, and
@@ -99,20 +99,20 @@ void cnd_destroy( cnd_t * cond );
    Initializes the variable in a way that a thread calling cnd_wait() on it
    would block.
 */
-int cnd_init( cnd_t * cond );
+_PDCLIB_PUBLIC int cnd_init( cnd_t * cond );
 
 /* Unblock one thread waiting on the condition variable.
    Returns thrd_success if successful, thrd_error if the request could not
    be honored.
 */
-int cnd_signal( cnd_t * cond );
+_PDCLIB_PUBLIC int cnd_signal( cnd_t * cond );
 
 /* TODO: Documentation.
    Returns thrd_success if successful, thrd_timedout if the specified time
    is reached without acquiring the resource, or thrd_error if the request
    could not be honored.
 */
-int cnd_timedwait( cnd_t * _PDCLIB_restrict cond, mtx_t * _PDCLIB_restrict mtx, const struct timespec * _PDCLIB_restrict ts );
+_PDCLIB_PUBLIC int cnd_timedwait( cnd_t * _PDCLIB_restrict cond, mtx_t * _PDCLIB_restrict mtx, const struct timespec * _PDCLIB_restrict ts );
 
 /* TODO: Documentation.
    Returns thrd_success if successful, thrd_error if the request could not
@@ -125,7 +125,7 @@ int cnd_wait( cnd_t * cond, mtx_t * mtx );
 /* Destroy mutex variable.
    No threads may be waiting on a mutex when it is destroyed.
 */
-void mtx_destroy( mtx_t * mtx );
+_PDCLIB_PUBLIC void mtx_destroy( mtx_t * mtx );
 
 /* Initialize mutex variable.
    Returns thrd_success if successful, thrd_error if the request could not
@@ -136,7 +136,7 @@ void mtx_destroy( mtx_t * mtx );
    mtx_plain | mtx_recursive -- recursive mutex
    mtx_timed | mtx_recursive -- recursive mutex supporting timeout
 */
-int mtx_init( mtx_t * mtx, int type );
+_PDCLIB_PUBLIC int mtx_init( mtx_t * mtx, int type );
 
 /* Try to lock the given mutex (blocking).
    Returns thrd_success if successful, thrd_error if the request could not
@@ -144,27 +144,27 @@ int mtx_init( mtx_t * mtx, int type );
    If the given mutex is non-recursive, it must not be already locked by
    the calling thread.
 */
-int mtx_lock( mtx_t * mtx );
+_PDCLIB_PUBLIC int mtx_lock( mtx_t * mtx );
 
 /* TODO: Documentation.
    Returns thrd_success if successful, thrd_timedout if the specified time
    is reached without acquiring the resource, or thrd_error if the request
    could not be honored.
 */
-int mtx_timedlock( mtx_t * _PDCLIB_restrict mtx, const struct timespec * _PDCLIB_restrict ts );
+_PDCLIB_PUBLIC int mtx_timedlock( mtx_t * _PDCLIB_restrict mtx, const struct timespec * _PDCLIB_restrict ts );
 
 /* Try to lock the given mutex (non-blocking).
    Returns thrd_success if successful, thrd_busy if the resource is already
    locked, or thrd_error if the request could not be honored.
 */
-int mtx_trylock( mtx_t * mtx );
+_PDCLIB_PUBLIC int mtx_trylock( mtx_t * mtx );
 
 /* Unlock the given mutex.
    Returns thrd_success if successful, thrd_error if the request could not
    be honored.
    The given mutex must be locked by the calling thread.
 */
-int mtx_unlock( mtx_t * mtx );
+_PDCLIB_PUBLIC int mtx_unlock( mtx_t * mtx );
 
 /* Thread functions */
 
@@ -175,12 +175,12 @@ int mtx_unlock( mtx_t * mtx );
    the created thread. (Identifiers may be reused afer a thread exited
    and was either detached or joined.)
 */
-int thrd_create( thrd_t * thr, thrd_start_t func, void * arg );
+_PDCLIB_PUBLIC int thrd_create( thrd_t * thr, thrd_start_t func, void * arg );
 
 /* Identify the calling thread.
    Returns the identifier of the calling thread.
 */
-thrd_t thrd_current( void );
+_PDCLIB_PUBLIC thrd_t thrd_current( void );
 
 /* Notify the OS to destroy all resources of a given thread once it
    terminates.
@@ -188,19 +188,19 @@ thrd_t thrd_current( void );
    be honored.
    The given thread must not been previously detached or joined.
 */
-int thrd_detach( thrd_t thr );
+_PDCLIB_PUBLIC int thrd_detach( thrd_t thr );
 
 /* Compare two thread identifiers for equality.
    Returns nonzero if both parameters identify the same thread, zero
    otherwise.
 */
-int thrd_equal( thrd_t thr0, thrd_t thr1 );
+_PDCLIB_PUBLIC int thrd_equal( thrd_t thr0, thrd_t thr1 );
 
 /* Terminate calling thread, set result code to res.
    When the last thread of a program terminates the program shall terminate
    normally as if by exit( EXIT_SUCCESS ).
 */
-_PDCLIB_Noreturn void thrd_exit( int res );
+_PDCLIB_PUBLIC _PDCLIB_Noreturn void thrd_exit( int res );
 
 /* Join the given thread with the calling thread.
    Returns thrd_success if successful, thrd_error if the request could not
@@ -208,7 +208,7 @@ _PDCLIB_Noreturn void thrd_exit( int res );
    Function blocks until the given thread terminates. If res is not NULL,
    the given thread's result code will be stored at that address.
 */
-int thrd_join( thrd_t thr, int * res );
+_PDCLIB_PUBLIC int thrd_join( thrd_t thr, int * res );
 
 /* Suspend the calling thread for the given duration or until a signal not
    being ignored is received.
@@ -218,10 +218,10 @@ int thrd_join( thrd_t thr, int * res );
    is not being ignored, the remaining time (duration minus actually elapsed
    time) shall be stored at that address.
 */
-int thrd_sleep( const struct timespec * duration, struct timespec * remaining );
+_PDCLIB_PUBLIC int thrd_sleep( const struct timespec * duration, struct timespec * remaining );
 
 /* Permit other threads to run. */
-void thrd_yield( void );
+_PDCLIB_PUBLIC void thrd_yield( void );
 
 /* Thread-specific storage functions */
 
@@ -229,20 +229,20 @@ void thrd_yield( void );
    Returns thrs_success if successful, thrd_error otherwise (in this case
    key is set to an undefined value).
 */
-int tss_create( tss_t * key, tss_dtor_t dtor );
+_PDCLIB_PUBLIC int tss_create( tss_t * key, tss_dtor_t dtor );
 
 /* Release all resources of a given thread-specific storage. */
-void tss_delete( tss_t key );
+_PDCLIB_PUBLIC void tss_delete( tss_t key );
 
 /* Returns the value for the current thread associated with the given key.
 */
-void * tss_get( tss_t key );
+_PDCLIB_PUBLIC void * tss_get( tss_t key );
 
 /* Sets the value associated with the given key for the current thread.
    Returns thrd_success if successful, thrd_error if the request could not
    be honored.
 */
-int tss_set( tss_t key, void * val );
+_PDCLIB_PUBLIC int tss_set( tss_t key, void * val );
 
 #ifdef __cplusplus
 }
