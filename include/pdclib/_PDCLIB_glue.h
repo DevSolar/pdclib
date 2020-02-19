8bf780d301f821fcdf58cdf0923c49f0494a27fc
Martin Baute <solar@rootdirectory.de>
2020-02-19 20:53:16 +0000

    Dual results for _PDCLIB_changemode()
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@872 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- include/pdclib/_PDCLIB_glue.h
+++ include/pdclib/_PDCLIB_glue.h
@@ -65,7 +65,9 @@ _PDCLIB_LOCAL int _PDCLIB_close( _PDCLIB_fd_t fd );
    argument (the argument being the value returned by _PDCLIB_filemode()),
    *without* closing the stream. See comments in example implementation
    for details. Return zero if the requested mode change is not supported
-   for this stream, non-zero on success.
+   for this stream and freopen() should try to close and reopen the stream;
+   return INT_MIN if the change is not supported and freopen() should NOT
+   try to close / reopen (i.e., fail). Return any other value on success.
 */
 _PDCLIB_LOCAL int _PDCLIB_changemode( struct _PDCLIB_file_t * stream, unsigned int mode );
 
