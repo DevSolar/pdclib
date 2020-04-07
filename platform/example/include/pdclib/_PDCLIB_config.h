0a53c48e0cc151d9cb723e6059743f1f3127c380
Martin Baute <solar@rootdirectory.de>
2020-04-07 12:38:12 +0000

    Identification of sig_atomic_t through SIG_ATOMIC_MAX.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@897 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- platform/example/include/pdclib/_PDCLIB_config.h
+++ platform/example/include/pdclib/_PDCLIB_config.h
@@ -236,8 +236,9 @@ struct _PDCLIB_lldiv_t
 
 /* An integer type that can be accessed as atomic entity (think asynchronous  */
 /* interrupts). In a freestanding environment, the type itself need not be    */
-/* defined, but its limits must. (Don't ask.)                                 */
-#define _PDCLIB_sig_atomic_t   __SIG_ATOMIC_TYPE__
+/* defined, but its limits must. (Don't ask.) GCC is so kind to predefine it, */
+/* but clang is only giving us its MAX value, so we use that to identify the  */
+/* type in _PDCLIB_int.h.                                                     */
 #define _PDCLIB_SIG_ATOMIC_MAX __SIG_ATOMIC_MAX__
 #define _PDCLIB_SIG_ATOMIC_MIN _PDCLIB_MIN_CALC( __SIG_ATOMIC_MAX__ )
 
