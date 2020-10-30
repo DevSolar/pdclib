74ea8e0bfd78146783bd90fbb740509e84de9806
Martin Baute <solar@rootdirectory.de>
2020-10-30 08:44:29 +0000

    bigint2()
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@959 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/_PDCLIB/_PDCLIB_bigint2.c
@@ -0,0 +1,72 @@
+/* _PDCLIB_bigint2( _PDCLIB_bigint_t *, unsigned )
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#ifndef REGTEST
+
+#include "pdclib/_PDCLIB_internal.h"
+
+#include <stdint.h>
+
+_PDCLIB_bigint_t * _PDCLIB_bigint2( _PDCLIB_bigint_t * bigint, unsigned value )
+{
+    int i;
+
+    bigint->size = ( value / 32 ) + 1;
+    bigint->data[ bigint->size - 1 ] = 1 << ( value - ( bigint->size - 1 ) * 32 );
+
+    for ( i = 0; i < bigint->size - 1; ++i )
+    {
+        bigint->data[ i ] = 0;
+    }
+
+    return bigint;
+}
+
+#endif
+
+#ifdef TEST
+
+#include "_PDCLIB_test.h"
+
+#include <stdint.h>
+
+int main( void )
+{
+#ifndef REGTEST
+    _PDCLIB_bigint_t big;
+    _PDCLIB_bigint_t * bigptr;
+    bigptr = _PDCLIB_bigint2( &big, 0 );
+    TESTCASE( bigptr == &big );
+    TESTCASE( big.size == 1 );
+    TESTCASE( big.data[0] == UINT32_C( 0x00000001 ) );
+    _PDCLIB_bigint2( &big, 3 );
+    TESTCASE( big.size == 1 );
+    TESTCASE( big.data[0] == UINT32_C( 0x00000008 ) );
+    _PDCLIB_bigint2( &big, 31 );
+    TESTCASE( big.size == 1 );
+    TESTCASE( big.data[0] == UINT32_C( 0x80000000 ) );
+    _PDCLIB_bigint2( &big, 32 );
+    TESTCASE( big.size == 2 );
+    TESTCASE( big.data[0] == UINT32_C( 0x00000000 ) );
+    TESTCASE( big.data[1] == UINT32_C( 0x00000001 ) );
+    _PDCLIB_bigint2( &big, 34 );
+    TESTCASE( big.size == 2 );
+    TESTCASE( big.data[0] == UINT32_C( 0x00000000 ) );
+    TESTCASE( big.data[1] == UINT32_C( 0x00000004 ) );
+    _PDCLIB_bigint2( &big, 63 );
+    TESTCASE( big.size == 2 );
+    TESTCASE( big.data[0] == UINT32_C( 0x00000000 ) );
+    TESTCASE( big.data[1] == UINT32_C( 0x80000000 ) );
+    _PDCLIB_bigint2( &big, 64 );
+    TESTCASE( big.size == 3 );
+    TESTCASE( big.data[0] == UINT32_C( 0x00000000 ) );
+    TESTCASE( big.data[1] == UINT32_C( 0x00000000 ) );
+    TESTCASE( big.data[2] == UINT32_C( 0x00000001 ) );
+#endif
+    return TEST_RESULTS;
+}
+
+#endif
