5fd970451381335ee26ea8e82d2efb5265c0d8b1
Martin Baute <solar@rootdirectory.de>
2020-11-17 09:42:55 +0000

    Again, newlib with a different (bit equally valid) interpretation.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@976 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- functions/locale/localeconv.c
+++ functions/locale/localeconv.c
@@ -25,7 +25,8 @@ int main( void )
     TESTCASE( ( lconv = localeconv() ) != NULL );
     TESTCASE( strcmp( lconv->decimal_point, "." ) == 0 );
     TESTCASE( strcmp( lconv->thousands_sep, "" ) == 0 );
-    TESTCASE( strcmp( lconv->grouping, "" ) == 0 );
+    printf( "lconv->grouping: \"%s\" (%02hhx %02hhx)\n", lconv->grouping, lconv->grouping[0], lconv->grouping[1] );
+    TESTCASE( ( strlen( lconv->grouping ) == 0 ) || ( ( strlen( lconv->grouping ) == 1 ) && ( lconv->grouping[0] == CHAR_MAX ) ) );
     return TEST_RESULTS;
 }
 
