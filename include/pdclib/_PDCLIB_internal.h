300dc2b955206fc6d77d76452442c2cd24271f07
solar <solar@bcf39385-58cc-4174-9fcf-14f50f90dd47>
2021-03-04 20:00:07 +0000

    Prototype fix for _PDCLIB_bigint_div().
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@997 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- include/pdclib/_PDCLIB_internal.h
+++ include/pdclib/_PDCLIB_internal.h
@@ -688,9 +688,6 @@ _PDCLIB_LOCAL _PDCLIB_bigint_t * _PDCLIB_bigint_mul_dig( _PDCLIB_bigint_t * lhs,
 /* Divides a given bigint by a given 32bit value. */
 _PDCLIB_LOCAL _PDCLIB_bigint_t * _PDCLIB_bigint_div_dig( _PDCLIB_bigint_t * lhs, _PDCLIB_bigint_digit_t rhs );
 
-/* Divides a given bigint by another given bigint. */
-_PDCLIB_LOCAL _PDCLIB_bigint_t * _PDCLIB_bigint_div( _PDCLIB_bigint_t * _PDCLIB_restrict lhs, _PDCLIB_bigint_t const * _PDCLIB_restrict rhs );
-
 /* Shifts a given bigint left by a given count of bits. */
 _PDCLIB_LOCAL _PDCLIB_bigint_t * _PDCLIB_bigint_shl( _PDCLIB_bigint_t * lhs, unsigned rhs );
 
@@ -699,6 +696,9 @@ _PDCLIB_LOCAL _PDCLIB_bigint_t * _PDCLIB_bigint_shl( _PDCLIB_bigint_t * lhs, uns
 
 /* Multiplies a given bigint with another given bigint. */
 _PDCLIB_LOCAL _PDCLIB_bigint_t * _PDCLIB_bigint_mul( _PDCLIB_bigint_t * _PDCLIB_restrict result, _PDCLIB_bigint_t const * _PDCLIB_restrict lhs, _PDCLIB_bigint_t const * _PDCLIB_restrict rhs );
+
+/* Divides a given bigint by another given bigint. */
+_PDCLIB_LOCAL _PDCLIB_bigint_t * _PDCLIB_bigint_div( _PDCLIB_bigint_t * _PDCLIB_restrict result, _PDCLIB_bigint_t const * _PDCLIB_restrict lhs, _PDCLIB_bigint_t const * _PDCLIB_restrict rhs );
 
 /* Queries */
 /* ------- */
