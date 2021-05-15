068f3b5a108c0d5d994fb488dec6cb69d140ba59
solar <solar@bcf39385-58cc-4174-9fcf-14f50f90dd47>
2021-05-15 14:21:17 +0000

    Issue #21: _PDCLIB_Noreturn not effective in C++ code. Suggested fix applied.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@1000 bcf39385-58cc-4174-9fcf-14f50f90dd47

--- include/pdclib/_PDCLIB_internal.h
+++ include/pdclib/_PDCLIB_internal.h
@@ -55,10 +55,14 @@ extern "C" {
 #endif
 
 /* noreturn enabled for C11 onward only */
-#if __STDC_VERSION__ < 201112L
-#define _PDCLIB_Noreturn
+#if defined( __cplusplus ) && __cplusplus >= 201103L
+#define _PDCLIB_Noreturn [[noreturn]]
 #else
+#if defined( __STDC_VERSION__ ) >= 201112L
 #define _PDCLIB_Noreturn _Noreturn
+#else
+#define _PDCLIB_Noreturn
+#endif
 #endif
 
 /* -------------------------------------------------------------------------- */
