99ec7ad30ee57ec02cfea8a591737515a676abb1
solar <solar@bcf39385-58cc-4174-9fcf-14f50f90dd47>
2021-07-01 16:10:05 +0000

    Preparing to pull apart the monolithic _PDCLIB_print.c.
    git-svn-id: https://srv183.svn-repos.de/dev34/pdclib/trunk@1008 bcf39385-58cc-4174-9fcf-14f50f90dd47

---
+++ functions/_PDCLIB/_PDCLIB_print_integer.c
@@ -0,0 +1,26 @@
+/* _PDCLIB_print_integer
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#ifndef REGTEST
+
+#include "pdclib/_PDCLIB_print.h"
+
+#endif
+
+#ifdef TEST
+
+#include <stddef.h>
+
+#include "_PDCLIB_test.h"
+
+int main( void )
+{
+#ifndef REGTEST
+#endif
+    return TEST_RESULTS;
+}
+
+#endif
---
+++ include/pdclib/_PDCLIB_print.h
@@ -0,0 +1,12 @@
+/* PDCLib printf logic <_PDCLIB_print.h>
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#ifndef _PDCLIB_PRINT_H
+#define _PDCLIB_PRINT_H _PDCLIB_PRINT_H
+
+#include "pdclib/_PDCLIB_internal.h"
+
+#endif
